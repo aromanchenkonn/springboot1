7799e9027eb099904c7995a3979a0590a4fc5a05
aromanchenkonn <a.romanchenko.nn@gmail.com>
2017-09-30 22:35:13 +0300

    add lombok project

---
+++ src/main/resources/public/js/main.js
@@ -0,0 +1,20 @@
+var app=angular.module("springDemo", []);
+
+app.controller("AppCtrl", function($scope, $http){
+    $scope.websites = [];
+    $http.get('http://localhost:8882/api/stackoverflow').success(function(data){
+    $scope.websites=data;
+    });
+});
+
+
+
+//app.controller("AppCtrl", function($scope){
+//    $scope.websites = [{
+//    iconImageUrl: 'https://www.santouka.co.jp/wp/wp-content/themes/santouka/img/home/icon-mark.png',
+//    id: 'stackoverflow',
+//    website: 'http://www.google.com',
+//    title: 'my name',
+//    description: 'my plot'
+//    }];
+//});
